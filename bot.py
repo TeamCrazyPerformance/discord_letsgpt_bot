824da26823aa623269bd84fd7ddd0c6d4f556c41
김현욱 <company@wiselight.kr>
2023-03-17 00:16:03 +0900

    feat: 대화로 ChatGPT에게 쿼리 보내기

--- bot.py
+++ bot.py
@@ -1,15 +1,28 @@
 import discord
-from discord.ext import commands
+import openai
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+client = discord.Client(intents=intents)
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+model = "gpt-3.5-turbo"
 
 
-@bot.command(name='hello')
-async def hello(ctx):
-    await ctx.send('Hello I am Bot!')
+@client.event
+async def on_message(query):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": query.content}
+    ]
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages
+    )
+    answer = response['choices'][0]['message']['content']
+    await query.channel.send(answer)
 
 
-bot.run(os.environ['DISCORD_TOKEN'])
+client.run(os.environ['DISCORD_TOKEN'])
