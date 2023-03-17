d8516e6434da654552b5683bedd8d2c7cbdcd0a0
김현욱 <company@wiselight.kr>
2023-03-18 01:23:38 +0900

    feat: 디스코드 쓰레드 생성 기능

--- bot.py
+++ bot.py
@@ -11,20 +11,42 @@ model = "gpt-3.5-turbo"
 
 
 @client.event
-async def on_message(query):
-    if query.author == client.user:
+async def on_message(message):
+    if message.author == client.user:
         return
 
-    messages = [
-        {"role": "user", "content": query.content}
-    ]
+    if message.channel.type == discord.ChannelType.text:
+        thread = await message.create_thread(name='New chat', auto_archive_duration=60)
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": message.content}
+            ]
+        )
+        answer = response['choices'][0]['message']['content']
+        await thread.send(answer)
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
-    answer = response['choices'][0]['message']['content']
-    await query.channel.send(answer)
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": f"'{message.content}'를 최대 세 단어로 요약"}
+            ]
+        )
+        answer = response['choices'][0]['message']['content']
+        await thread.edit(
+            name=answer
+        )
+    elif message.channel.type == discord.ChannelType.public_thread:
+        messages = [
+            {"role": "user", "content": message.content}
+        ]
+
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages
+        )
+        answer = response['choices'][0]['message']['content']
+        await message.channel.send(answer)
 
 
 client.run(os.environ['DISCORD_TOKEN'])
